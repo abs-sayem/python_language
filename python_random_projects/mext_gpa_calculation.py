3cea4df722c0209d3403afba6652298b9c697980
ABS-Sayem <abssayem121194@gmail.com>
2022-09-19 16:53:11 +0600

    added new project mext_gpa_calculation

---
+++ python_random_projects/mext_gpa_calculation.py
@@ -0,0 +1,6 @@
+courses = int(input("Enter the number of 3-credit courses: "))
+#print(courses)
+numbers = []
+for i in range(courses):
+    numbers.append(int(input(f"Enter course {i} number: ")))
+#print(numbers)
