4eb2a8f378b6343a81c4b6f3b2b51074abb04101
ABS-Sayem <abssayem121194@gmail.com>
2022-09-21 11:56:04 +0600

    solve some issues

--- python_random_projects/mext_gpa_calculation.py
+++ python_random_projects/mext_gpa_calculation.py
@@ -1,15 +1,18 @@
 # Take the course numbers:
+import numbers
+
+
 def getNumbers(courses):
     course_numbers = []
     for i in range(courses):
-        course_numbers.append(int(input(f"Enter course {i} number: ")))
+        course_numbers.append(float(input(f"Enter course {i} number: ")))
     return(course_numbers)
 
 # Take the course numbers:
-def getGrades(grades):
+def getGrades(courses):
     course_grades = []
-    for i in range(grades):
-        course_grades.append(int(input(f"Enter course {i} grade: ")))
+    for i in range(courses):
+        course_grades.append(float(input(f"Enter course {i} grade: ")))
     return(course_grades)
 
 # Compare and assigns the numbers equbalance to MEXT standards:
@@ -21,7 +24,7 @@ def courseNumber(numbers):
         elif(numbers[i] <= 79 and numbers[i] >= 70): course_number += (3*2)
         elif(numbers[i] <= 69 and numbers[i] >= 60): course_number += (3*1)
         else: course_number += (3*0)
-    print("Your MEXT Standard GPA: ", course_number/(courses*3))
+    print("Your MEXT Standard GPA: ", course_number/(no_courses*3))
 
 def courseGrade(grades):
     course_grade = 0
@@ -33,15 +36,15 @@ def courseGrade(grades):
     print("Your MEXT Standard GPA: ", course_grade/(len(grades)*3))
 
 # How many courses he has:
-courses = int(input("Enter the number of 3-credit courses: "))
+no_courses = int(input("Enter the number of 3-credit courses: "))
 #print(courses)
 # Chose number based or grade based
-print("Choose your prefered type:\n\t1) Number\n\t2) Grade")
+print("Choose your prefered type:\n\t1) Grade\n\t2) Number")
 type = int(input("What type of input you want to provide: "))
 
 if(type == 1):
-    grades = getGrades()
-    courseGrade()
-else:
-    courses = getNumbers()
-    courseNumber()
+    grades = getGrades(no_courses)
+    courseGrade(grades)
+elif(type == 2):
+    numbers = getNumbers(no_courses)
+    courseNumber(numbers)
